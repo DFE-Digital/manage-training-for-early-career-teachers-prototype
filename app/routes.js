440db2282af1a87829557e1ae48c868374167c42
Frankie Roberto <frankie@frankieroberto.com>
2023-09-28 13:57:54 +0100

    Update content

--- app/routes.js
+++ app/routes.js
@@ -114,6 +114,17 @@ router.get('/early-career-teachers/:id/change-dp-lp', (req, res) => {
 })
 
 
+router.get('/early-career-teachers/:id/support-contacted', (req, res) => {
+  const { id } = req.params
+
+  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
+
+  res.render('ects/support-contacted', {
+    id,
+    teacher
+  })
+})
+
 router.get('/early-career-teachers/:id/transfer', (req, res) => {
   const { id } = req.params
 
