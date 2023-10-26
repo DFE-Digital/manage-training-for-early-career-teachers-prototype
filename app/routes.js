1cfca260d354f3f4e47d127bdf1c6e4a54d2039d
Frankie Roberto <frankie@frankieroberto.com>
2023-10-26 15:07:27 +0100

    More WIP

--- app/routes.js
+++ app/routes.js
@@ -6,269 +6,6 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
-router.get('/participants', (req, res) => {
-
-  let mentors = JSON.parse(JSON.stringify(req.session.data.mentors))
-  let teachers = JSON.parse(JSON.stringify(req.session.data.teachers))
-
-  let ectsBeingTrained = teachers.filter((teacher) => (!teacher.completedDate && !teacher.noLongerTraining))
-  let ectsCompleted = teachers.filter((teacher) => teacher.completedDate)
-  let ectsNoLongerTraining = teachers.filter((teacher) => teacher.noLongerTraining)
-
-  let teachersWithoutMentors = ectsBeingTrained.filter((teacher) => !teacher.mentorId)
-
-  for (mentor of mentors) {
-    mentor.earlyCareerTeachers = []
-
-    for (teacher of req.session.data.teachers.filter((teacher) =>
-      teacher.mentorId === mentor.id &&
-      !teacher.completedDate &&
-      !teacher.noLongerTraining
-    )) {
-      mentor.earlyCareerTeachers.push(JSON.parse(JSON.stringify(teacher)))
-    }
-  }
-
-  for (teacher of ectsBeingTrained) {
-    if (teacher.mentorId) {
-      teacher.mentor = JSON.parse(JSON.stringify(mentors.find((mentor) => mentor.id === teacher.mentorId)))
-    }
-  }
-
-
-  ectsBeingTrained = ectsBeingTrained.sort(function(teacherA, teacherB) {
-    if (!teacherA.inductionStartDate) {
-      return -1
-    }
-
-    if (!teacherB.inductionStartDate) {
-      return 1
-    }
-
-    if (teacherA.inductionStartDate > teacherB.inductionStartDate) {
-      return -1
-    } else {
-      return 1
-    }
-  })
-
-  res.render('participants', {
-    ectsBeingTrained,
-    ectsCompleted,
-    ectsNoLongerTraining,
-    mentors,
-    teachersWithoutMentors
-  })
-
-})
-
-router.get('/early-career-teachers/:id', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  let mentor = null
-  if (teacher.mentorId) {
-    mentor = req.session.data.mentors.find((mentor) => mentor.id === teacher.mentorId)
-  }
-
-  res.render('early-career-teacher', {
-    id,
-    teacher,
-    mentor
-  })
-})
-
-router.get('/early-career-teachers/:id/change-trn', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('ects/change-trn', {
-    id,
-    teacher
-  })
-})
-
-router.get('/early-career-teachers/:id/change-dob', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('ects/change-dob', {
-    id,
-    teacher
-  })
-})
-
-router.get('/early-career-teachers/:id/change-dp-lp', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('ects/change-dp-lp', {
-    id,
-    teacher
-  })
-})
-
-
-router.get('/early-career-teachers/:id/support-contacted', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('ects/support-contacted', {
-    id,
-    teacher
-  })
-})
-
-router.get('/early-career-teachers/:id/transfer', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('transfer', {
-    id,
-    teacher
-  })
-})
-
-router.get('/early-career-teachers/:id/transfer-reason', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('transfer-reason', {
-    id,
-    teacher
-  })
-})
-
-router.get('/early-career-teachers/:id/transfer-check', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('transfer-check', {
-    id,
-    teacher
-  })
-})
-
-router.get('/early-career-teachers/:id/transfer-contact', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('transfer-contact', {
-    id,
-    teacher
-  })
-})
-
-router.post('/early-career-teachers/:id/answer-transfer-reason', (req, res) => {
-  const { id } = req.params
-  const { transferReason } = req.session.data
-
-  if (transferReason === "permanent") {
-    res.redirect(`/early-career-teachers/${id}/transfer-where`)
-  } else {
-    res.redirect(`/early-career-teachers/${id}/transfer-check`)
-  }
-
-})
-
-router.get('/early-career-teachers/:id/transfer-where', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('transfer-where', {
-    id,
-    teacher
-  })
-})
-
-router.post('/early-career-teachers/:id/confirm-transfer', (req, res) => {
-  const { id } = req.params
-  const { transferReason, transferDate } = req.session.data
-  const dateNow = new Date()
-
-  const transferDateAsDate = new Date(transferDate.year, transferDate.month, transferDate.day)
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  teacher.leftSchoolOn = req.session.data.transferDate
-  req.session.data.transferDate = {}
-
-  if ( transferDateAsDate < new Date ) {
-    teacher.noLongerTraining = true
-  }
-
-  if (transferReason === "temporary") {
-    teacher.temporarilyLeft = true
-  }
-
-  if (transferReason === "permanent") {
-    res.redirect(`/early-career-teachers/${id}/transfer-confirmed`)
-  } else {
-    res.redirect(`/early-career-teachers/${id}/transfer-contact`)
-  }
-
-
-})
-
-router.get('/early-career-teachers/:id/transfer-confirmed', (req, res) => {
-  const { id } = req.params
-
-  const teacher = req.session.data.teachers.find((teacher) => teacher.id === id)
-
-  res.render('transfer-confirmed', {
-    id,
-    teacher
-  })
-})
-
-router.get('/mentors/:id', (req, res) => {
-  const { id } = req.params
-
-  let mentor = JSON.parse(JSON.stringify(req.session.data.mentors.find(function(mentor) {
-    return mentor.id === id
-  })))
-
-  mentor.earlyCareerTeachers = []
-
-  for (teacher of req.session.data.teachers.filter((teacher) =>
-      teacher.mentorId === mentor.id &&
-      !teacher.completedDate &&
-      !teacher.noLongerTraining
-    )) {
-      mentor.earlyCareerTeachers.push(JSON.parse(JSON.stringify(teacher)))
-    }
-
-  res.render('mentor', {
-    id,
-    mentor
-  })
-})
-
-
-router.post('/add-ect/answer-new-programme', (req, res) => {
-
-  const newProvider = req.session.data.newProvider
-
-  if (newProvider === "other") {
-    res.redirect('/add-ect/transfer-contact-support')
-  } else {
-    res.redirect('/add-ect/check')
-  }
-
-})
-
-
 router.post('/ects/answer-training-programme', (req, res) => {
 
   const answer = req.body.trainingProgramme
@@ -279,4 +16,98 @@ router.post('/ects/answer-training-programme', (req, res) => {
     res.redirect('/ects/appropriate-body')
   }
 
-})
+});
+
+
+router.post('/ects/confirm', (req, res) => {
+
+  let data = req.session.data;
+  req.session.data.ects ||= []
+
+  let inductionStartDate;
+  if (data.startTerm == "Start of the Autumn 2023 term") {
+    inductionStartDate = {day: 1, month: 9, year: 2023}
+  } else if (data.startTerm == "Start of the Spring 2024 term") {
+    inductionStartDate = {day: 3, month: 1, year: 2024}
+  } else if (data.startTerm == "Start of the Summer 2024 term") {
+    inductionStartDate = {day: 17, month: 4, year: 2024}
+  } else {
+    inductionStartDate = data.startDate
+  }
+
+  let mentors = [
+    {
+      from: inductionStartDate,
+      mentor: { name: data.mentor, id: "6454575" }
+    }
+  ]
+
+  let ect = {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    trn: data.trn,
+    dob: data.dob,
+    inductionStartDate: inductionStartDate,
+    schoolName: data.schoolName,
+    inductionTutor: data.inductionTutor,
+    mentors: mentors,
+    appropriateBody: data.appropriateBody,
+    teachingSchoolHub: data.teachingSchoolHub,
+    trainingProgramme: data.trainingProgramme,
+    leadProvider: data.leadProvider
+  }
+
+  req.session.data.ects.push(ect)
+
+  delete data.firstName
+  delete data.lastName
+  delete data.trn
+  delete data.dob
+  delete data.schoolName
+  delete data.startTerm
+  delete data.startDate
+  delete data.inductionTutor
+  delete data.mentor
+  delete data.appropriateBody
+  delete data.teachingSchoolHub
+  delete data.trainingProgramme
+  delete data.leadProvider
+
+  res.redirect('/ects')
+});
+
+
+router.get('/ects/:ectId/show', (req, res) => {
+
+  let ect = req.session.data.ects.find((ect) => ect.trn == req.params.ectId)
+
+  res.render('ects/show', {
+    ect
+  })
+});
+
+
+router.get('/ects/:ectId/change-mentor', (req, res) => {
+
+  let ect = req.session.data.ects.find((ect) => ect.trn == req.params.ectId)
+
+  let currentMentor
+
+  if (ect.mentors.length > 0) {
+    currentMentor = ect.mentors.find((mentor) => mentor.to == undefined)
+  }
+
+  res.render('ects/change-mentor', {
+    ect,
+    currentMentor
+  })
+});
+
+router.get('/ects/:ectId/new-mentor', (req, res) => {
+
+  let ect = req.session.data.ects.find((ect) => ect.trn == req.params.ectId)
+
+  res.render('ects/new-mentor', {
+    ect
+  })
+});
