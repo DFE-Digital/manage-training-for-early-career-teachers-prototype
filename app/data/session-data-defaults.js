1d3bee3f9c7d28619e99f895015cc24aaf7b142b
Frankie Roberto <frankie@frankieroberto.com>
2023-11-17 16:05:59 +0000

    Bugfix (#43)

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -97,8 +97,13 @@ module.exports = {
       inductionStartDate: "2023-06-20",
       mentors: [
         {
+          id: "NSY835",
+          from: "2023-06-20",
+          to: "2023-09-20"
+        },
+        {
           id: "NDY639",
-          from: "2023-06-20"
+          from: "2023-09-20"
         }
       ]
     },
