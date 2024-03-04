d0c1ba0f2a60e5116571c968eaf011153e5dd874
Tom <thomas.h.brown@icloud.com>
2024-03-04 14:51:52 +0000

    Added participant dir for LP change journey

--- app/routes/lead-provider.js
+++ app/routes/lead-provider.js
@@ -4,13 +4,13 @@ module.exports = router => {
 
     router.post('/lead-provider/change-lp-for',(req,res) => {
         if (req.body.change.leadProvFor == 'Participant') {
-            res.redirect('/lead-provider/contacted-lead-provider')    
+            res.redirect('/lead-provider/participant/participant-email')    
         } else {
             res.redirect('/lead-provider/agreed-change-existing-provider')    
         }
     })
 
-    router.post('/lead-provider/participant-email',(req,res) => {
+    router.post('/lead-provider/participant/participant-email',(req,res) => {
 
         if (req.query.returnUrl) {
             res.redirect(req.query.returnUrl)    
@@ -19,19 +19,19 @@ module.exports = router => {
         }       
     })
 
-    router.post('/lead-provider/contacted-lead-provider',(req,res) => {
+    router.post('/lead-provider/participant/contacted-lead-provider',(req,res) => {
         if (req.query.returnUrl) {
             res.redirect(req.query.returnUrl)    
         } else {
-            res.redirect('/lead-provider/new-lead-provider')
+            res.redirect('new-lead-provider')
         }       
     })
 
-    router.post('/lead-provider/new-lead-provider',(req,res) => {
+    router.post('/lead-provider/participant/new-lead-provider',(req,res) => {
         if (req.query.returnUrl) {
             res.redirect(req.query.returnUrl)    
         } else {
-            res.redirect('/lead-provider/check-participant')
+            res.redirect('check-participant')
         }       
     })
 }
