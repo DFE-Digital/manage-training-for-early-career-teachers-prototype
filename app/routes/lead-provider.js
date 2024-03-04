8796c70349155d57e189af16ef8f1547071ae943
Tom <thomas.h.brown@icloud.com>
2024-03-04 15:57:49 +0000

    Added dir for Academic year LP change journey

--- app/routes/lead-provider.js
+++ app/routes/lead-provider.js
@@ -1,12 +1,12 @@
 module.exports = router => {
 
-
+    //Change LP participant routes
 
     router.post('/lead-provider/change-lp-for',(req,res) => {
         if (req.body.change.leadProvFor == 'Participant') {
             res.redirect('/lead-provider/participant/participant-email')    
         } else {
-            res.redirect('/lead-provider/agreed-change-existing-provider')    
+            res.redirect('/lead-provider/academic-year/agreed-change-existing-provider')    
         }
     })
 
@@ -34,4 +34,41 @@ module.exports = router => {
             res.redirect('check-participant')
         }       
     })
+
+
+
+    //Change LP participant routes
+
+    router.post('/lead-provider/academic-year/agreed-change-existing-provider',(req,res) => {
+        if (req.query.returnUrl) {
+            res.redirect(req.query.returnUrl)    
+        }
+        else if (req.body.agreedWith.providers == 'Yes') {
+            res.redirect('already-contacted-lead-provider')    
+        }
+        else {
+            res.redirect('contact-existing-providers')    
+        }
+    })
+
+
+
+
+    router.post('/lead-provider/academic-year/already-contacted-lead-provider',(req,res) => {
+        if (req.body.contacted.leadProvider == 'Yes') {
+            res.redirect('new-lead-provider')    
+        } else {
+            res.redirect('what-to-do-next')    
+        }     
+    })
+
+    router.post('/lead-provider/academic-year/new-lead-provider',(req,res) => {
+        if (req.query.returnUrl) {
+            res.redirect(req.query.returnUrl)    
+        } else {
+            res.redirect('check-answers-academic-year')
+        }
+    })
+
+
 }
