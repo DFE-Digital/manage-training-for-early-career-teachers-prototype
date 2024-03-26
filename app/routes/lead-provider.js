55b6ae2137b916ba49c8e1db93a4f347619e0d42
Tom <thomas.h.brown@icloud.com>
2024-03-26 12:04:15 +0000

    Participant change Lep provider journey - moved email confirm AFTER agree with LP and contacted LP question pages

--- app/routes/lead-provider.js
+++ app/routes/lead-provider.js
@@ -1,13 +1,38 @@
 module.exports = router => {
 
-    //Change LP participant routes
+    //Change LP PARTICIPANT routes
 
     router.post('/lead-provider/change-lp-for',(req,res) => {
         if (req.body.change.leadProvFor == 'Participant') {
-            res.redirect('/lead-provider/participant/participant-email')    
+            res.redirect('/lead-provider/participant/agreed-change-existing-provider')    
         } else {
             res.redirect('/lead-provider/academic-year/agreed-change-existing-provider')    
         }
+    })
+    
+
+    router.post('/lead-provider/participant/agreed-change-existing-provider',(req,res) => {
+        if (req.query.returnUrl) {
+            res.redirect(req.query.returnUrl)    
+        }
+        else if (req.body.agreedWithParticipant.providers == 'Yes') {
+            res.redirect('already-contacted-lead-provider')    
+        }
+        else {
+            res.redirect('contact-existing-providers')    
+        }
+    })
+
+    router.post('/lead-provider/participant/already-contacted-lead-provider',(req,res) => {
+        if (req.query.returnUrl) {
+            res.redirect(req.query.returnUrl)    
+        }
+        else if (req.body.contacted.leadProvider == 'Yes') {
+            res.redirect('participant-email')    
+        }
+        else {
+            res.redirect('what-to-do-next')    
+        }     
     })
 
     router.post('/lead-provider/participant/participant-email',(req,res) => {
@@ -15,21 +40,11 @@ module.exports = router => {
         if (req.query.returnUrl) {
             res.redirect(req.query.returnUrl)    
         } else {
-            res.redirect('contacted-lead-provider')    
+            res.redirect('new-lead-provider')    
         }       
     })
 
-    router.post('/lead-provider/participant/contacted-lead-provider',(req,res) => {
-        if (req.query.returnUrl) {
-            res.redirect(req.query.returnUrl)    
-        }
-        else if (req.body.contacted.leadProvider == 'Yes') {
-            res.redirect('new-lead-provider')    
-        }
-        else {
-            res.redirect('what-to-do-next')    
-        }       
-    })
+ 
 
     router.post('/lead-provider/participant/new-lead-provider',(req,res) => {
         if (req.query.returnUrl) {
@@ -41,7 +56,7 @@ module.exports = router => {
 
 
 
-    //Change LP participant routes
+    /////////// Change LP ACADEMIC YEAR routes 
 
     router.post('/lead-provider/academic-year/agreed-change-existing-provider',(req,res) => {
         if (req.query.returnUrl) {
