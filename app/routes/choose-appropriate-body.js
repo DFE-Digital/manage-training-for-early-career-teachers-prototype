a5f45a14a15fcaa02cae1fecdc0d5e073d5babbb
Tom <thomas.h.brown@icloud.com>
2024-04-04 15:47:56 +0100

    Added british-school-overseas.html istip-or-teaching-school-hub.html teaching-school-hub-or-new-appropriate-body.html + route for /registration/choose-appropriate-body/independent-school

--- app/routes/choose-appropriate-body.js
+++ app/routes/choose-appropriate-body.js
@@ -1,6 +1,6 @@
 module.exports = router => {
 
-    //Change DELIVERY PARTNER participant routes
+    ///////////////   Choose APPROPRIATE BODY routes  /////////////////////////
 
     router.post('/registration/choose-appropriate-body/appointed-appropriate-body',(req,res) => {
         if (req.query.returnUrl) {
@@ -11,6 +11,18 @@ module.exports = router => {
         }
         else {
             res.redirect('no-appropriate-body')    
+        }   
+    })
+
+    router.post('/registration/choose-appropriate-body/independent-school',(req,res) => {
+        if (req.query.returnUrl) {
+            res.redirect(req.query.returnUrl)    
+        }
+        else if (req.body.iStipOrTeachingSchoolHub == 'Yes') {
+            res.redirect('istip-or-teaching-school-hub')    
+        }
+        else {
+            res.redirect('british-school-overseas')    
         }   
     })
 
